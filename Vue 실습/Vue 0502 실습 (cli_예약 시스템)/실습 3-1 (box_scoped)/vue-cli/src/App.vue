a0631f3e8045046d13458d64c529f1b59a21dfca
ddingdu <zizu4256@naver.com>
2023-05-02 17:39:28 +0900

    23.05.02

---
+++ Vue 실습/Vue 0502 실습 (cli_예약 시스템)/실습 3-1 (box_scoped)/vue-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HomeView />
+    <AboutView />
+  </div>
+</template>
+
+<script>
+import AboutView from './views/AboutView.vue'
+import HomeView from './views/HomeView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AboutView,
+    HomeView,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
