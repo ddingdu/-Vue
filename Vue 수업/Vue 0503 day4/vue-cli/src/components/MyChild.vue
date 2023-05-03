2396dae790ab8cff8f9229e57869cafc4fe6c504
ddingdu <zizu4256@naver.com>
2023-05-03 17:56:02 +0900

    23.05.03

---
+++ Vue 수업/Vue 0503 day4/vue-cli/src/components/MyChild.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h3>This is child component</h3>
+    <p>{{ staticProps }}</p>
+    <p>{{ myProps }}</p>
+    <button @click="childToParent">클릭!</button>
+    <!-- 변수와 input창 연동시키기, 엔터키 눌렀다 뗄때 childInput 실행 -->
+    <input type="text" v-model="childInputData" @keyup.enter="childInput">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyChild',
+  data: function () {
+    return {
+      childInputData: null,
+    }
+  },
+  props: {
+    staticProps: String,
+    myProps: String,
+  },
+  methods: {
+    childToParent: function() {
+      this.$emit('child-to-parent', 'child Data')
+    },
+    // 이벤트를 발생 시킴, 사용자가 입력한 데이터를 받아서 보내줌
+    childInput: function() {
+      this.$emit('child-input', this.childInputData)
+      this.childInputData=""
+    }
+  }
+}
+</script>
+
+<style>
+  
+</style>
---
+++ Vue 수업/Vue 0503 day4/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="border">
+    <h1>This is MyComponent</h1>
+    <MyChild static-props="component(부모)에서 child로"
+    :my-props="dynamicProps"
+    @child-to-parent="parentGetEvent"
+    @child-input="getDynamicData"
+    />
+    <!-- 자식이 보낸 이벤트가 발생하면 getDynamicData메서드 실행 -->
+
+    <!-- <MyChild static-props="1"
+    :my-props="'1'"
+    /> -->
+  </div>
+</template>
+
+<script>
+import MyChild from '@/components/MyChild'
+
+export default {
+  name: 'MyComponent',
+  components: {
+    MyChild,
+  },
+  data: function() {
+    return{
+      dynamicProps: "It's in data"
+    }
+  },
+  methods: {
+    parentGetEvent(inputData) {
+      console.log('자식 컴폰넌트에서 발생한 이벤트!')
+      console.log(`child에서 보낸 ${inputData}를 받음!`)
+    },
+    getDynamicData(input) {
+      console.log(`child에서 입력한 ${input}을 출력!`)
+    }
+  }
+}
+
+</script>
+
+<style>
+  .border {
+    border: solid;
+  }
+</style>
